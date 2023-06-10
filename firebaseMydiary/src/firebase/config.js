931f22e81d0c4426a658588c1c50017f94a31c25
yonainthefish <yonaisgood@gmail.com>
2023-06-11 06:20:02 +0900

    리액트공부

---
+++ firebaseMydiary/src/firebase/config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {getAuth} from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+//firebase를 초기화합니다
+const app = initializeApp(firebaseConfig);
+//firebase를 초기화 합니다
+const appFireStore = getFirestore(app);
+// 인증을 초기화 합니다.
+const appAuth = getAuth();
+
+export {appFireStore, appAuth}
